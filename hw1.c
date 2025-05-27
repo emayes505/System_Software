28e07ae287ee282f852df68aad756eec50254f14
Erik Mayes <emayes@Eriks-MacBook-Air.local>
2025-05-27 17:38:50 -0400

    update

--- hw1.c
+++ hw1.c
@@ -8,6 +8,19 @@ typedef struct IR{
 } IR;
 
 
-int main(void) {
-int n = 5;
+int main(int argc, char **argv) {
+printf("start");
+if(argc != 2) {
+    printf("Usage <./hw1><input#.txt>");
+    return 1;
+}
+char **input = argv[1];
+FILE *File;
+int stack[500];
+File = fopen(&input, "r");
+if(File == NULL) {
+    perror("invalid file name");
+    return 2;
+}
+printf("end");
 }
