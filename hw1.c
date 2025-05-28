8178484ad1326cbd9d1e9f9eda8d55ee830797a1
Erik Mayes <emayes528@gmail.com>
2025-05-28 12:34:13 -0400

    setup

--- hw1.c
+++ hw1.c
@@ -9,18 +9,28 @@ typedef struct IR{
 
 
 int main(int argc, char **argv) {
-printf("start");
+
 if(argc != 2) {
     printf("Usage <./hw1><input#.txt>");
     return 1;
 }
-char **input = argv[1];
-FILE *File;
-int stack[500];
-File = fopen(&input, "r");
-if(File == NULL) {
-    perror("invalid file name");
+
+FILE *fp = fopen(argv[1], "r");
+
+if(fp == NULL) {
+    perror("Error opening file");
     return 2;
 }
-printf("end");
+
+int pas[500];
+int num;
+int i = 10;
+
+while(fscanf(fp,"%d",&num) == 1) {
+    pas[i++] = num;
+}
+
+for(int k = 10; k < i; k++) {
+    printf("%d ", pas[k]);
+}
 }
